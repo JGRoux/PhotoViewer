e57215fc53d54afcd4472fa5345b6f79a781b581
JGRoux <jroux@ece.fr>
2015-02-02 15:13:20 +0100

    Test

---
+++ PhotoViewer/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoViewer
+{
+    class Album
+    {
+        private String Name { get; set; }
+    }
+}
